ba5f4c8d480439c2cb0337682372b95b2603b984
xiaozhou2017 <853522011@qq.com>
2019-07-29 19:45:50 +0800

    日志信息查看 接口参数 返回code 说明

--- demo3/src/main/java/com/example/demo/demoController/DemoController.java
+++ demo3/src/main/java/com/example/demo/demoController/DemoController.java
@@ -16,14 +16,15 @@ public class DemoController {
 
     private static Logger log = LoggerFactory.getLogger(DemoController.class);
 
+
     /**
-     * 输入的手机键值 0-9   如 2   2,3   8,9,6   10,99,12 形式
-     * @param
-     * @return
+     * 输入的手机键值字符串 0-9   如 2   2,3   8,9,6   10,99,12 形式
+     * @param digitstr
+     * @return 200 返回成功 401 参数有误 500 内部错误
      */
 
     @RequestMapping(value = "/letterbynum",method = RequestMethod.GET)
-        public Map getletterbynum(String digitstr){ //没有封装Bean,对数组传值不友好，暂时采用string 规则传值
+    public Map getletterbynum(String digitstr){ //没有封装Bean,对数组传值不友好，暂时采用string 规则传值
         Map map=new HashMap();
         List<String> ls;
         map.put("list",null);
@@ -31,7 +32,7 @@ public class DemoController {
         map.put("message","返回成功！");
         try {
             if(digitstr.length()==0){
-                map.put("code",200);
+                map.put("code",401);
                 map.put("message","参数为空！");
                 return map;
             }
@@ -46,12 +47,14 @@ public class DemoController {
                 }
             }
             ls=demoService.doexcute(digitsstr.toString());
-
+            log.info("===传入参数========={}",digitsstr.toString());
             map.put("list",ls);
+            log.info("===传出结果========={}",map);
             return map;
         }catch (Exception e){
             map.put("code",500);
             map.put("message","出错了 "+e.getMessage());
+            log.info("系统错误~~~~~~~~~~~~~");
             return map;
         }
     }
